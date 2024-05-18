a74de504bc035ee440072d1f9a7acdccea148ae3
Tomasz Wysocki <0xdaydream@proton.me>
2024-05-18 04:51:49 +0200

    Change to reading chunks using buffer

--- main.go
+++ main.go
@@ -3,9 +3,13 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io"
+	"log"
 	"os"
 	"strings"
 )
+
+const chunkSize = 16
 
 func main() {
 	flag.Parse()
@@ -17,49 +21,90 @@ func main() {
 }
 
 func readFile(name string) {
-	data, err := os.ReadFile(name)
+	f, err := os.Open(name)
 	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
+		log.Fatal(err)
 	}
-
-	for i := 0; i < len(data); i += 16 {
-		var padding string
-		end := i + 16
-
-		if end > len(data) {
-			end = len(data)
+	defer f.Close()
+	buf := make([]byte, chunkSize)
+	for i := 0; ; i++ {
+		n, err := f.Read(buf)
+		if n > 2342345 {
+			fmt.Println("haha")
 		}
-
-		bytes := data[i:end]
-		fmt.Printf("%08x: ", i)
-
-		var fourBytesSlice []string
-
-		for j := 0; j < end-i; j += 2 {
-			endInner := j + 2
-			if endInner > end-i {
-				endInner = end - i
+		if err != nil && err != io.EOF {
+			log.Fatal(err)
+		}
+		if err == io.EOF {
+			break
+		}
+		fmt.Printf("%08x: ", i*16)
+		padding := 39
+		for i, b := range buf[:n] {
+			fmt.Printf("%02x", b)
+			padding -= 2
+			if i%2 != 0 && i != len(buf[:n])-1 {
+				fmt.Print(" ")
+				padding -= 1
 			}
-			fourBytesSlice = append(fourBytesSlice, fmt.Sprintf("%x", bytes[j:endInner]))
 		}
-
-		fmt.Printf("%v", strings.Trim(fmt.Sprint(fourBytesSlice), "[]"))
-
-		if len(fourBytesSlice) < 8 {
-			padding = fmt.Sprintf(strings.Repeat(" ", (16-(end-i))*2+8-len(fourBytesSlice)))
-		}
-		fmt.Printf("%v", padding)
-
-		var bytesString string
-		for _, b := range bytes {
+		fmt.Print(strings.Repeat(" ", padding))
+		fmt.Print("  ")
+		for _, b := range buf[:n] {
 			if string(b) == "\n" || b == 0 {
-				bytesString += fmt.Sprint(".")
+				fmt.Print(".")
 				continue
 			}
-			bytesString += fmt.Sprint(string(b))
+			fmt.Print(string(b))
 		}
-		fmt.Printf("  %v", bytesString)
 		fmt.Println()
 	}
 }
+
+// func readFile(name string) {
+// 	data, err := os.ReadFile(name)
+// 	if err != nil {
+// 		fmt.Println(err)
+// 		os.Exit(1)
+// 	}
+
+// 	for i := 0; i < len(data); i += 16 {
+// 		var padding string
+// 		end := i + 16
+
+// 		if end > len(data) {
+// 			end = len(data)
+// 		}
+
+// 		bytes := data[i:end]
+// 		fmt.Printf("%08x: ", i)
+
+// 		var fourBytesSlice []string
+
+// 		for j := 0; j < end-i; j += 2 {
+// 			endInner := j + 2
+// 			if endInner > end-i {
+// 				endInner = end - i
+// 			}
+// 			fourBytesSlice = append(fourBytesSlice, fmt.Sprintf("%x", bytes[j:endInner]))
+// 		}
+
+// 		fmt.Printf("%v", strings.Trim(fmt.Sprint(fourBytesSlice), "[]"))
+
+// 		if len(fourBytesSlice) < 8 {
+// 			padding = fmt.Sprintf(strings.Repeat(" ", (16-(end-i))*2+8-len(fourBytesSlice)))
+// 		}
+// 		fmt.Printf("%v", padding)
+
+// 		var bytesString string
+// 		for _, b := range bytes {
+// 			if string(b) == "\n" || b == 0 {
+// 				bytesString += fmt.Sprint(".")
+// 				continue
+// 			}
+// 			bytesString += fmt.Sprint(string(b))
+// 		}
+// 		fmt.Printf("  %v", bytesString)
+// 		fmt.Println()
+// 	}
+// }
