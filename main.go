3d59938742083b9922401ba93b198d9714a9af42
Tomasz Wysocki <0xdaydream@proton.me>
2024-05-19 16:35:09 +0200

    Refactor base logic

--- main.go
+++ main.go
@@ -15,6 +15,8 @@ const chunkSize = 16
 var octetsPerGroup = flag.Int("g", 2, "number of octets per group in normal output. Default 2 (-e: 4)")
 var isLittleEndian = flag.Bool("e", false, "little-endian dump")
 
+var padding int
+
 func main() {
 	flag.Parse()
 	if flag.NArg() != 1 {
@@ -54,50 +56,16 @@ func readFile(name string) {
 
 		bytesLine := buf[:n]
 
-		fmt.Printf("%08x: ", i*16)
-		padding := 32 + 16 / *octetsPerGroup
+		fmt.Printf("%08x: ", i*chunkSize)
+		padding = 32 + 16 / *octetsPerGroup
 
 		if 16%*octetsPerGroup == 0 {
 			padding--
 		}
 
-		if *isLittleEndian {
-			groupCount := int(math.Ceil(float64(len(bytesLine)) / float64(*octetsPerGroup)))
-			for i := range groupCount {
-				for j := *octetsPerGroup - 1; j >= 0; j-- {
-					if j+i**octetsPerGroup >= len(bytesLine) {
-						fmt.Print("  ")
-						padding -= 2
-						continue
-					}
-					fmt.Printf("%02x", bytesLine[j+i**octetsPerGroup])
-					padding -= 2
-				}
-				if i != groupCount-1 {
-					fmt.Print(" ")
-					padding -= 1
-				}
-			}
-		} else {
-			for i, b := range bytesLine {
-				fmt.Printf("%02x", b)
-				padding -= 2
-				if (i+1)%*octetsPerGroup == 0 && i != len(bytesLine)-1 {
-					fmt.Print(" ")
-					padding -= 1
-				}
-			}
-		}
-
-		fmt.Print(strings.Repeat(" ", padding))
-		fmt.Print("  ")
-		for _, b := range bytesLine {
-			if string(b) == "\n" || b == 0 {
-				fmt.Print(".")
-				continue
-			}
-			fmt.Print(string(b))
-		}
+		printHex(bytesLine)
+		fmt.Printf("%s  ", strings.Repeat(" ", padding))
+		printText(bytesLine)
 		fmt.Println()
 	}
 }
@@ -107,4 +75,44 @@ func isPowerOfTwo(n int) bool {
 		return true
 	}
 	return false
+}
+
+func printHex(bytes []byte) {
+	if *isLittleEndian {
+		groupCount := int(math.Ceil(float64(len(bytes)) / float64(*octetsPerGroup)))
+		for i := range groupCount {
+			for j := *octetsPerGroup - 1; j >= 0; j-- {
+				if j+i**octetsPerGroup >= len(bytes) {
+					fmt.Print("  ")
+					padding -= 2
+					continue
+				}
+				fmt.Printf("%02x", bytes[j+i**octetsPerGroup])
+				padding -= 2
+			}
+			if i != groupCount-1 {
+				fmt.Print(" ")
+				padding -= 1
+			}
+		}
+	} else {
+		for i, b := range bytes {
+			fmt.Printf("%02x", b)
+			padding -= 2
+			if (i+1)%*octetsPerGroup == 0 && i != len(bytes)-1 {
+				fmt.Print(" ")
+				padding -= 1
+			}
+		}
+	}
+}
+
+func printText(bytes []byte) {
+	for _, b := range bytes {
+		if string(b) == "\n" || b == 0 {
+			fmt.Print(".")
+			continue
+		}
+		fmt.Print(string(b))
+	}
 }
